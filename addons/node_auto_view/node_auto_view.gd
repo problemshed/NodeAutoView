5c975fef8b7fa3b88bbe64c5e84f1864a0c7c090
Chase <51481058+problemshed@users.noreply.github.com>
2025-04-05 14:04:57 +0100

    Update node_auto_view.gd

--- addons/node_auto_view/node_auto_view.gd
+++ addons/node_auto_view/node_auto_view.gd
@@ -9,12 +9,12 @@ func _exit_tree() -> void:
 
 func _on_selection_changed() -> void:
 	var _nodes := EditorInterface.get_selection().get_selected_nodes()
-	var all_2d := true
-	var all_3d := true
+	var _all_2d := true
+	var _all_3d := true
 	for _node in _nodes:
 		if (not _node is Node2D) and (not _node is Control):
-			all_2d = false
+			_all_2d = false
 		if not _node is Node3D:
-			all_3d = false
-	if all_2d: EditorInterface.set_main_screen_editor("2D")
-	elif all_3d: EditorInterface.set_main_screen_editor("3D")
+			_all_3d = false
+	if _all_2d: EditorInterface.set_main_screen_editor("2D")
+	elif _all_3d: EditorInterface.set_main_screen_editor("3D")
